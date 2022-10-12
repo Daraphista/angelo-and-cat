85af1ba4c819a49acd3c815871d1663888001179
Angelo Mendoza <angelo@modiphy.net>
2022-10-12 22:22:33 +0800

    Create animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
         7.5: "7.5deg",
         15: "15deg",
       },
+      animation: {
+        "float-up": "float 3s linear infinite",
+      },
+      keyframes: {
+        float: {
+          "0%": { transform: "translateY(100vh) scale(0)" },
+          "100%": { transform: "translateY(-10vh) scale(2)" },
+        },
+      },
     },
   },
   plugins: [],
